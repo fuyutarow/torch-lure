5e26ffdfdbb3e0bb90ce6d865f82b31b30341088
fuyutarow <fuyutarow@gmail.com>
2024-07-06 08:22:59 +0900

    chore: Update torchlure version to 0.2407.0, add dependencies, and clean up code

--- README.md
+++ README.md
@@ -43,10 +43,28 @@ lure.CosineNoiseScheduler(max_beta=0.999, s=0.008, num_timesteps=1000):
 
 
 ```py
-from torchlure.datasets import OfflineRLDataset, D4RLDataset
+from torchlure.datasets import MinariEpisodeDataset, MinariTrajectoryDataset
 
-# Initial usage with download
-# %%
+env = gym.make("Hopper-V4")
+minari_dataset = MinariEpisodeDataset("2048.2407.2")
+minari_dataset.create(env, n_episodes=100)
+minari_dataset.info()
+
+traj_dataset = MinariTrajectoryDataset(minari_dataset, traj_len=20)
+
+ep = traj_dataset[2]
+ep["observations"].shape, ep["actions"].shape, ep["rewards"].shape, ep[
+    "terminations"
+].shape, ep["truncate"].shape
+
+ep = traj_dataset[[3, 8, 15]]
+ep = traj_dataset[np.arange(16)]
+ep = traj_dataset[torch.arange(16)]
+ep = traj_dataset[-16:]
+
+```
+
+<!-- # %%
 dataset = D4RLDataset(
     dataset_id= "hopper-medium-expert-v2.2405",
     d4rl_name= "hopper-medium-expert-v2",
@@ -56,7 +74,5 @@ dataset = D4RLDataset(
 # if you are download it once
 dataset = D4RLDataset(
     dataset_id= "hopper-medium-expert-v2.2405",
-)
-```
-
-See all datasets [here](https://github.com/pytorch/rl/blob/3a7cf6af2a08089f11e0ed8cad3dd1cea0e253fb/torchrl/data/datasets/d4rl_infos.py)
+) -->
+<!-- See all datasets [here](https://github.com/pytorch/rl/blob/3a7cf6af2a08089f11e0ed8cad3dd1cea0e253fb/torchrl/data/datasets/d4rl_infos.py) -->
--- src/torchlure/modules/__init__.py
+++ src/torchlure/modules/__init__.py
@@ -1,11 +1,7 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-#
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree.
-
+import einops as ein
 import torch
 from torch import Tensor, nn
+from torchtyping import TensorType
 
 
 class RMSNorm(nn.Module):
@@ -41,3 +37,38 @@ class RMSNorm(nn.Module):
             x_fp32 * torch.rsqrt(x_fp32.pow(2).mean(-1, keepdim=True) + self.eps)
         ).type_as(x)
         return x_normed * self.scale
+
+
+class ReLUKANLayer(nn.Module):
+    def __init__(
+        self, input_size: int, g: int, k: int, output_size: int, train_ab: bool = True
+    ):
+        super().__init__()
+        self.g, self.k, self.r = g, k, 4 * g * g / ((k + 1) * (k + 1))
+        self.input_size, self.output_size = input_size, output_size
+
+        phase_low = torch.arange(-k, g, dtype=torch.float32) / g
+        phase_height = phase_low + (k + 1) / g
+        self.phase_low = nn.Parameter(
+            phase_low.repeat(input_size, 1),
+            requires_grad=train_ab,
+        )
+        self.phase_height = nn.Parameter(
+            phase_height.repeat(input_size, 1),
+            requires_grad=train_ab,
+        )
+
+        self.equal_size_conv = nn.Conv2d(1, output_size, (g + k, input_size))
+
+    def forward(
+        self, x: TensorType["B", "input_size"]
+    ) -> TensorType["B", "output_size"]:
+        x = x.unsqueeze(-1)
+        x1 = F.relu(x - self.phase_low)
+        x2 = F.relu(self.phase_height - x)
+        x = x1 * x2 * self.r
+        x = x * x
+        x = ein.rearrange(x, "... in gk -> ... 1 gk in")
+        x = self.equal_size_conv(x)
+        x = ein.rearrange(x, "... out 1 1 -> ... out")
+        return x
